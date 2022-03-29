391c0b81febbc6d535824d8736e2eb78b10fc7e5
Wojtek Lukowski <wojtek_lukowski@yahoo.com>
2022-03-29 16:25:09 +0200

    file structure

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+const key = config.GOOGLE_API_KEY;
+
+
+navigator.geolocation.getCurrentPosition(success);
+
+function success (pos) {
+  const crd = pos.coords;
+  // console.log('current position: ', crd.latitude, crd.longitude);
+  getLocation(crd.latitude, crd.longitude);
+};
+
+async function getLocation(lat, lng) {
+  try {
+    const api = `https://maps.googleapis.com/maps/api/geocode/json?latlng=${lat},${lng}&key=`;
+    const data = await (await fetch(api)).json();
+    // console.log(data);
+    // console.log(data.results[0]);
+    // console.log(data.results[0].formatted_address);
+    // console.log(data.results[0].address_components[7]);
+    console.log('current location: ', data.results[0].address_components[7].long_name);
+    const locationCountry = data.results[0].address_components[7].long_name.toLowerCase().replace(' ', '-');
+    getCountry(locationCountry, daysBack);
+  } catch(err) {
+    getCountry('poland', daysBack);
+  }
+};
